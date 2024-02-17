24114b14406bd4731d4931e393ddd53eaf95d5d2
Eric Madureira <ericmadu416@gmail.com>
2024-02-17 17:03:57 -0300

    feat: footer and quotes count

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import Image from "next/image"
+import Link from "next/link"
 
 // Assets
 import dogbackground1 from "@/assets/dogbg1.png"
 import dogbackground2 from "@/assets/dogbg2.png"
+
+// Components
+import Footer from "@/components/Footer"
 
 // Styles
 import "./globals.css"
@@ -22,6 +26,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const generatedQuotes = 0
+
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -29,6 +35,13 @@ export default function RootLayout({
           <Image src={dogbackground1} className={styles.dogbackground1} height='300' alt='dogbackground1' />
           <Image src={dogbackground2} className={styles.dogbackground2} height='300' alt='dogbackground2' />
           {children}
+          <Footer>
+            <>
+              Quotes generated: {generatedQuotes}
+              <br />
+              Developed with 🧠 by <Link href="https://www.linkedin.com/in/eric-madureira/" target="_blank" rel="noopener noreferrer">Eric Madureira</Link>
+            </>
+          </Footer>
         </main>
       </body>
     </html>
