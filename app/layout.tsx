be5f4bd0a7006b3f17fc3d8764760932c3c71e0b
Eric Madureira <ericmadu416@gmail.com>
2024-02-18 15:17:02 -0300

    feat: layout composing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import Image from "next/image"
-import Link from "next/link"
 
-// Assets
-import dogbackground1 from "@/assets/dogbg1.png"
-import dogbackground2 from "@/assets/dogbg2.png"
+import Link from "next/link"
 
 // Components
 import Footer from "@/components/Footer"
+import { BackgroundImage1,BackgroundImage2 } from "@/components/BackgroundImage"
 
 // Styles
 import "./globals.css"
@@ -32,16 +29,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className={styles['main-container']}>
-          <Image src={dogbackground1} className={styles.dogbackground1} height='300' alt='dogbackground1' />
-          <Image src={dogbackground2} className={styles.dogbackground2} height='300' alt='dogbackground2' />
           {children}
-          <Footer>
-            <>
-              Quotes generated: {generatedQuotes}
-              <br />
-              Developed with 🧠 by <Link href="https://www.linkedin.com/in/eric-madureira/" target="_blank" rel="noopener noreferrer">Eric Madureira</Link>
-            </>
-          </Footer>
+
+          <BackgroundImage1 />
+          <BackgroundImage2 />
+
+          {/* <QuoteGenerator /> */}
+
+          <Footer generatedQuotes={generatedQuotes} />
         </main>
       </body>
     </html>
