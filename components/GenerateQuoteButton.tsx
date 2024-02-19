5eb0b3dc2b6b66e6dde6d91b4ce9ad90872947f6
Eric Madureira <ericmadu416@gmail.com>
2024-02-19 12:28:59 -0300

    feat: GenerateQuoteButton

---
+++ components/GenerateQuoteButton.tsx
@@ -0,0 +1,9 @@
+import styles from './GenerateQuoteButton.module.css'
+
+export default function GenerateQuoteButton() {
+  return (
+    <button className={styles['generate-quote-button']}>
+      Make a Quote
+    </button>
+  )
+}
