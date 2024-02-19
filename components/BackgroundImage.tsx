f9629b67f10b3aa13aacf3d69585d120fa68487e
Eric Madureira <ericmadu416@gmail.com>
2024-02-19 11:34:08 -0300

    fix: style in bg images

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image"
 
+import styles from './BackgroundImage.module.css'
+
 // Assets
 import dogbackground1 from "@/assets/dogbg1.png"
 import dogbackground2 from "@/assets/dogbg2.png"
 
-const BackgroundImage1 = () => (<Image src={dogbackground1} className="dogbackground1" height='300' alt='dogbackground1' />)
-const BackgroundImage2 = () => (<Image src={dogbackground2} className="dogbackground2" height='300' alt='dogbackground2' />)
+const BackgroundImage1 = () => (<Image src={dogbackground1} className={styles.dogbackground1} height='300' alt='dogbackground1' />)
+const BackgroundImage2 = () => (<Image src={dogbackground2} className={styles.dogbackground2} height='300' alt='dogbackground2' />)
 
 export { BackgroundImage1, BackgroundImage2 }
