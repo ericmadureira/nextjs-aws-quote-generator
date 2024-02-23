fda3778aff1c379645657d15c2e1282e2504de66
Eric Madureira <ericmadu416@gmail.com>
2024-02-23 10:21:32 -0300

    feat: move update count logic to component

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,15 @@
 'use client'
-import { useEffect, useState } from "react"
 import { Amplify } from 'aws-amplify'
-import { generateClient } from 'aws-amplify/api'
 
 import amplifyconfig from '@/src/amplifyconfiguration.json'
-import { quotesQueryName } from "@/src/graphql/queries"
 import QuoteGenerator from '@/components/QuoteGenerator'
-import Footer from "@/components/Footer"
-
-interface UpdateGeneratedQuotes{
-  id: string
-  queryName: string
-  quotesGenerated: number
-  createdAt: string
-  updatedAt: string
-}
 
 Amplify.configure(amplifyconfig)
-const amplifyAPI = generateClient()
 
 export default function Home() {
-  const [generatedQuotes, setGeneratedQuotes] = useState<number | null>(0)
-
-  const updateGeneratedQuotes = async () => {
-    try {
-      const response = await amplifyAPI.graphql<UpdateGeneratedQuotes>({
-        query: quotesQueryName,
-        authMode: "iam",
-        variables: {
-          queryName: "LIVE"
-        }
-      })
-      console.log("response", response)
-      // setGeneratedQuotes(response.data?.quotesQueryName?.quotesGenerated)
-    } catch (error) {
-      console.log("Error updating generated quotes:", error)
-    }
-  }
-
-  useEffect(() => {
-    updateGeneratedQuotes()
-  }, [])
-
   return (
     <div>
       <QuoteGenerator />
-      <Footer generatedQuotes={generatedQuotes} />
     </div>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,62 @@
+'use client'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
+import { GraphQLResult } from 'aws-amplify/api'
+
+import { quotesQueryName } from '@/src/graphql/queries'
+import amplifyAPI from '@/amplify-client'
 
 import styles from './Footer.module.css'
 
-interface FooterProps {
-  generatedQuotes: number | null
+interface UpdateGeneratedQuotes{
+  id: string
+  queryName: string
+  quotesGenerated: number
+  createdAt: string
+  updatedAt: string
 }
 
-export default function Footer({ generatedQuotes }: FooterProps) {
+// Type guard
+function isGraphQLResultForQuotes(response: any): response is GraphQLResult<{
+  quotesQueryName: {
+    items: [UpdateGeneratedQuotes]
+  }
+}>{
+  return response.data && response.data.quotesQueryName && response.data.quotesQueryName.items
+}
+
+export default function Footer() {
+  const [generatedQuotes, setGeneratedQuotes] = useState<number | null>(null)
+
+  const updateGeneratedQuotes = async () => {
+    try {
+      const response = await amplifyAPI.graphql<UpdateGeneratedQuotes>({
+        query: quotesQueryName,
+        authMode: "iam",
+        variables: {
+          queryName: "LIVE"
+        }
+      })
+
+      if(!isGraphQLResultForQuotes(response)) {
+        throw new Error("Unexpected response from API.graphql")
+      }
+
+      if(!response.data){
+        throw new Error("Response data is undefined")
+      }
+
+      const receivedQuotesCount = response.data.quotesQueryName.items[0].quotesGenerated
+      setGeneratedQuotes(receivedQuotesCount)
+    } catch (error) {
+      console.log("Error updating generated quotes:", error)
+    }
+  }
+
+  useEffect(() => {
+    updateGeneratedQuotes()
+  }, [])
+
   return (
     <div className={styles['footer-container']}>
       Quotes generated: {generatedQuotes}
